485e1e1e5b774eafe4860c5e2c0bb2387f690133
f-perejoguine <fedorpavlovitch.perejoguine@students.fhnw.ch>
2019-10-16 17:52:05 +0200

    Initial commit. Executing java compilation works.

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+In the config file, specify
+    Java program file location
+    Timeout duration
+    Array of substrings. Lines with these will be commented out or deleted.
+    java bin path.
+
+Upon running this main file:
+    1. Replace strings as per rule (Remove system.out.println and the like)
+    2. In working dir, create folder for each test.
+    3. Compile java file and put result in to each test folder,
+        as well as the corresponding input file for the test
+    4. Run the program. On timeout, terminate and produce timeout error.
+        On error, print full stack trace.
+    5. On finish without errors, compare result to out file, print either
+        success or error with received and expected output written down.
+    6. Do this in parallel for all tests.
+"""
+import json
+import subprocess
+import os
+
+with open('config.json', 'r') as outfile:
+    config = json.load(outfile)
+    argslist = [
+        'javac',
+        config['program-location'],
+        '-d',
+        os.path.dirname(os.path.abspath(__file__))
+    ]
+    executable_path = config['java-dir'] + '\\javac.exe'
+    print(argslist)
+    subprocess.Popen(argslist, executable=executable_path)
+    
